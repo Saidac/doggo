0894a9993ab9cd6cdb0dd6c902735592c3b4e791
William Saidac <william.saidac@gmail.com>
2018-11-22 13:26:20 +0100

    works with string

--- my-app/src/Card.js
+++ my-app/src/Card.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 
 const cardImgStyle = {
     height: 'auto',
@@ -12,17 +12,15 @@ class Card extends React.Component {
     return <img className="card-img-top" alt="" src={this.props.src} style={cardImgStyle}/>
   }
 }
-
+/*
 Card.propTypes = {
     items: PropTypes.array.isRequired
 }
-
+*/
 export default Card;
 
 /*
 <div className="card">
 <img className="card-img-top" alt="" src="https://images.dog.ceo/breeds/boxer/n02108089_1.jpg" style={cardImgStyle}></img>
 </div>
-
-
 */
